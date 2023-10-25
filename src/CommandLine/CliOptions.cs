f378d32c51312ffd91c492dccc2298f6de0f53f7
Ross King <ross.king@altavec.com>
2023-10-25 14:46:32 +1100

    ✨ add standard verbosity option

---
+++ src/CommandLine/CliOptions.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CliOptions.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.CommandLine;
+
+/// <summary>
+/// The <see cref="CliOption"/> values.
+/// </summary>
+public static class CliOptions
+{
+    /// <summary>
+    /// The verbosity option.
+    /// </summary>
+    public static readonly CliOption<VerbosityOptions> VerbosityOption = new("--verbosity", "-v")
+    {
+        HelpName = "LEVEL",
+        DefaultValueFactory = _ => VerbosityOptions.normal,
+        Description = "Set the verbosity level. Allowed values are q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic].",
+        Recursive = true,
+    };
+}
---
+++ src/CommandLine/VerbosityOptions.cs
@@ -0,0 +1,64 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="VerbosityOptions.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.CommandLine;
+
+/// <summary>
+/// The verbosity options.
+/// </summary>
+[Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:Element should begin with upper-case letter", Justification = "This is required for parsing")]
+public enum VerbosityOptions
+{
+    /// <summary>
+    /// Quiet verbosity option.
+    /// </summary>
+    quiet,
+
+    /// <summary>
+    /// Short quiet verbosity option.
+    /// </summary>
+    q,
+
+    /// <summary>
+    /// Minimal verbosity option.
+    /// </summary>
+    minimal,
+
+    /// <summary>
+    /// Shor minimal verbosity option.
+    /// </summary>
+    m,
+
+    /// <summary>
+    /// Normal verbosity option.
+    /// </summary>
+    normal,
+
+    /// <summary>
+    /// Short normal verbosity option.
+    /// </summary>
+    n,
+
+    /// <summary>
+    /// Detailed verbosity option.
+    /// </summary>
+    detailed,
+
+    /// <summary>
+    /// Short detailed verbosity option.
+    /// </summary>
+    d,
+
+    /// <summary>
+    /// Diagnostic verbosity option.
+    /// </summary>
+    diagnostic,
+
+    /// <summary>
+    /// Short diagnostic verbosity option.
+    /// </summary>
+    diag,
+}
