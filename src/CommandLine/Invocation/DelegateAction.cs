4020d3e2a0f4992daee170b59104df1daeb911dc
Ross King <ross.king@altavec.com>
2023-10-25 15:37:08 +1100

    ✨ added option to specify sync and async

--- src/CommandLine/Invocation/DelegateAction.cs
+++ src/CommandLine/Invocation/DelegateAction.cs
@@ -16,18 +16,50 @@ public static class DelegateAction
     /// </summary>
     /// <param name="command">The command.</param>
     /// <param name="action">The delegate.</param>
+    /// <param name="func">The asynchronous delegate.</param>
+    /// <param name="preferSynchronous">Set to <see langword="true"/> to prefer <paramref name="action"/> when no action is present.</param>
+    public static void SetHandlers(
+        CliCommand command,
+        Action<ParseResult> action,
+        Func<ParseResult, Task> func,
+        bool preferSynchronous = false)
+    {
+        if (command.Action is AsynchronousCliAction asyncAction)
+        {
+            command.Action = new Handlers.DelegateNestedAsynchronousCliAction(func, asyncAction);
+        }
+        else if (command.Action is SynchronousCliAction syncAction)
+        {
+            command.Action = new Handlers.DelegateNestedSynchronousCliAction(action, syncAction);
+        }
+
+        command.Action ??= preferSynchronous
+            ? new Handlers.DelegateSynchronousCliAction(action)
+            : new Handlers.DelegateAsynchronousCliAction(func);
+
+        foreach (var subCommand in command.Subcommands)
+        {
+            SetHandlers(subCommand, action);
+        }
+    }
+
+    /// <summary>
+    /// Sets the handlers.
+    /// </summary>
+    /// <param name="command">The command.</param>
+    /// <param name="action">The delegate.</param>
     public static void SetHandlers(CliCommand command, Action<ParseResult> action)
     {
         if (command.Action is AsynchronousCliAction asyncAction)
         {
-            command.Action = new Handlers.Action.DelegateNestedAsynchronousCliAction(action, asyncAction);
+            command.Action = new Handlers.DelegateNestedAsynchronousCliAction(parseResult => Task.Run(() => action(parseResult)), asyncAction);
         }
         else if (command.Action is SynchronousCliAction syncAction)
         {
-            command.Action = new Handlers.Action.DelegateNestedSynchronousCliAction(action, syncAction);
+            command.Action = new Handlers.DelegateNestedSynchronousCliAction(action, syncAction);
         }
 
-        command.Action ??= new Handlers.Action.DelegateSynchronousCliAction(action);
+        command.Action ??= new Handlers.DelegateSynchronousCliAction(action);
 
         foreach (var subCommand in command.Subcommands)
         {
@@ -44,10 +76,10 @@ public static class DelegateAction
     {
         if (command.Action is AsynchronousCliAction asyncAction)
         {
-            command.Action = new Handlers.Func.DelegateNestedAsynchronousCliAction(func, asyncAction);
+            command.Action = new Handlers.DelegateNestedAsynchronousCliAction(func, asyncAction);
         }
 
-        command.Action ??= new Handlers.Func.DelegateAsynchronousCliAction(func);
+        command.Action ??= new Handlers.DelegateAsynchronousCliAction(func);
 
         foreach (var subCommand in command.Subcommands)
         {
@@ -57,54 +89,39 @@ public static class DelegateAction
 
     private static class Handlers
     {
-        public static class Action
+        public sealed class DelegateSynchronousCliAction(Action<ParseResult> action) : SynchronousCliAction
         {
-            public sealed class DelegateSynchronousCliAction(Action<ParseResult> action) : SynchronousCliAction
+            public override int Invoke(ParseResult parseResult)
             {
-                public override int Invoke(ParseResult parseResult)
-                {
-                    action(parseResult);
-                    return 0;
-                }
-            }
-
-            public sealed class DelegateNestedAsynchronousCliAction(Action<ParseResult> action, AsynchronousCliAction cliAction) : Invocation.NestedAsynchronousCliAction(cliAction)
-            {
-                public override Task<int> InvokeAsync(ParseResult parseResult, CancellationToken cancellationToken = default)
-                {
-                    action(parseResult);
-                    return base.InvokeAsync(parseResult, cancellationToken);
-                }
-            }
-
-            public sealed class DelegateNestedSynchronousCliAction(Action<ParseResult> action, SynchronousCliAction cliAction) : NestedSynchronousCliAction(cliAction)
-            {
-                public override int Invoke(ParseResult parseResult)
-                {
-                    action(parseResult);
-                    return base.Invoke(parseResult);
-                }
+                action(parseResult);
+                return 0;
             }
         }
 
-        public static class Func
+        public sealed class DelegateAsynchronousCliAction(Func<ParseResult, Task> func) : AsynchronousCliAction
         {
-            public sealed class DelegateAsynchronousCliAction(Func<ParseResult, Task> func) : AsynchronousCliAction
+            public async override Task<int> InvokeAsync(ParseResult parseResult, CancellationToken cancellationToken = default)
             {
-                public async override Task<int> InvokeAsync(ParseResult parseResult, CancellationToken cancellationToken = default)
-                {
-                    await func(parseResult).ConfigureAwait(false);
-                    return 0;
-                }
+                await func(parseResult).ConfigureAwait(false);
+                return 0;
             }
+        }
 
-            public sealed class DelegateNestedAsynchronousCliAction(Func<ParseResult, Task> func, AsynchronousCliAction cliAction) : NestedAsynchronousCliAction(cliAction)
+        public sealed class DelegateNestedAsynchronousCliAction(Func<ParseResult, Task> func, AsynchronousCliAction cliAction) : NestedAsynchronousCliAction(cliAction)
+        {
+            public async override Task<int> InvokeAsync(ParseResult parseResult, CancellationToken cancellationToken = default)
             {
-                public async override Task<int> InvokeAsync(ParseResult parseResult, CancellationToken cancellationToken = default)
-                {
-                    await func(parseResult).ConfigureAwait(false);
-                    return await base.InvokeAsync(parseResult, cancellationToken).ConfigureAwait(false);
-                }
+                await func(parseResult).ConfigureAwait(false);
+                return await base.InvokeAsync(parseResult, cancellationToken).ConfigureAwait(false);
+            }
+        }
+
+        public sealed class DelegateNestedSynchronousCliAction(Action<ParseResult> action, SynchronousCliAction cliAction) : NestedSynchronousCliAction(cliAction)
+        {
+            public override int Invoke(ParseResult parseResult)
+            {
+                action(parseResult);
+                return base.Invoke(parseResult);
             }
         }
     }
