b450bad7bf5e680d46b6dab9f10686d9fd20d84d
Ross King <ross.king@altavec.com>
2023-10-26 09:42:25 +1100

    ✨ add logging extensions/classes

---
+++ src/CommandLine.Logging/Logging/CliConfigurationLogger.cs
@@ -0,0 +1,44 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CliConfigurationLogger.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.CommandLine.Logging;
+
+using Microsoft.Extensions.Logging;
+
+/// <summary>
+/// The <see cref="CliConfiguration"/> <see cref="ILogger"/>.
+/// </summary>
+/// <param name="configuration">The configuration.</param>
+/// <param name="level">The log level.</param>
+internal class CliConfigurationLogger(CliConfiguration configuration, LogLevel level) : ILogger
+{
+    /// <inheritdoc/>
+    public IDisposable BeginScope<TState>(TState state) => throw new NotSupportedException();
+
+    /// <inheritdoc/>
+    public bool IsEnabled(LogLevel logLevel) => logLevel >= level;
+
+    /// <inheritdoc/>
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+        if (!this.IsEnabled(logLevel))
+        {
+            return;
+        }
+
+        var writer = logLevel switch
+        {
+            LogLevel.Critical or LogLevel.Error => configuration.Error,
+            _ => configuration.Output,
+        };
+
+        writer.WriteLine(formatter(state, exception));
+        if (exception is not null)
+        {
+            writer.WriteLine(exception.ToString());
+        }
+    }
+}
---
+++ src/CommandLine.Logging/LoggingExtensions.cs
@@ -0,0 +1,30 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="LoggingExtensions.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.CommandLine.Logging;
+
+/// <summary>
+/// <see cref="Microsoft.Extensions.Logging"/> extensions.
+/// </summary>
+public static class LoggingExtensions
+{
+    /// <summary>
+    /// Gets the log level.
+    /// </summary>
+    /// <param name="parseResult">The parse result.</param>
+    /// <returns>The log level.</returns>
+    public static Microsoft.Extensions.Logging.LogLevel GetLogLevel(this ParseResult parseResult) => parseResult.GetValue(CliOptions.VerbosityOption).GetLogLevel();
+
+    private static Microsoft.Extensions.Logging.LogLevel GetLogLevel(this VerbosityOptions options) => options switch
+    {
+        VerbosityOptions.q or VerbosityOptions.quiet => Microsoft.Extensions.Logging.LogLevel.Error,
+        VerbosityOptions.m or VerbosityOptions.minimal => Microsoft.Extensions.Logging.LogLevel.Warning,
+        VerbosityOptions.n or VerbosityOptions.normal => Microsoft.Extensions.Logging.LogLevel.Information,
+        VerbosityOptions.d or VerbosityOptions.detailed => Microsoft.Extensions.Logging.LogLevel.Debug,
+        VerbosityOptions.diag or VerbosityOptions.diagnostic => Microsoft.Extensions.Logging.LogLevel.Trace,
+        _ => throw new ArgumentOutOfRangeException(nameof(options)),
+    };
+}
