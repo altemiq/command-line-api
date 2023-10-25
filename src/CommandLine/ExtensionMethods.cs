8afca4676c1c21df5d4b5be4be156a06a7bc4461
Ross King <ross.king@altavec.com>
2023-10-25 14:46:18 +1100

    ✨ add `Required` extensions

--- src/CommandLine/ExtensionMethods.cs
+++ src/CommandLine/ExtensionMethods.cs
@@ -12,10 +12,91 @@ namespace System.CommandLine;
 public static class ExtensionMethods
 {
     /// <summary>
-    /// Gets the command from the symbol result.
+    /// Gets the value from the parse result or throws an exception.
+    /// </summary>
+    /// <typeparam name="T">The type of value.</typeparam>
+    /// <param name="parseResult">The parse result.</param>
+    /// <param name="name">The name of the Symbol for which to get a value.</param>
+    /// <returns>The value.</returns>
+    /// <exception cref="ArgumentNullException">Result from <paramref name="name"/> is <see langword="null"/>.</exception>
+    [Runtime.CompilerServices.MethodImpl(Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public static T GetRequiredValue<T>(this ParseResult parseResult, string name)
+        where T : notnull => parseResult.GetValue<T>(name).ThrowIfNull();
+
+    /// <summary>
+    /// Gets the value from the parse result or throws an exception.
+    /// </summary>
+    /// <typeparam name="T">The type of value.</typeparam>
+    /// <param name="parseResult">The parse result.</param>
+    /// <param name="argument">The argument for which to get a value.</param>
+    /// <returns>The value.</returns>
+    /// <exception cref="ArgumentNullException">Result from <paramref name="argument"/> is <see langword="null"/>.</exception>
+    [Runtime.CompilerServices.MethodImpl(Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public static T GetRequiredValue<T>(this ParseResult parseResult, CliArgument<T> argument)
+        where T : notnull => parseResult.GetValue(argument).ThrowIfNull();
+
+    /// <summary>
+    /// Gets the value from the parse result or throws an exception.
+    /// </summary>
+    /// <typeparam name="T">The type of value.</typeparam>
+    /// <param name="parseResult">The parse result.</param>
+    /// <param name="option">The option for which to get a value.</param>
+    /// <returns>The value.</returns>
+    /// <exception cref="ArgumentNullException">Result from <paramref name="option"/> is <see langword="null"/>.</exception>
+    [Runtime.CompilerServices.MethodImpl(Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    public static T GetRequiredValue<T>(this ParseResult parseResult, CliOption<T> option)
+        where T : notnull => parseResult.GetValue(option).ThrowIfNull();
+
+    /// <summary>
+    /// Gets the result for the specified argument.
+    /// </summary>
+    /// <param name="parseResult">The parse result.</param>
+    /// <param name="argument">The argument for which to find a result.</param>
+    /// <returns>A result for the specified argument.</returns>
+    /// <exception cref="ArgumentNullException">Failed to find the result.</exception>
+    public static Parsing.ArgumentResult GetRequiredResult(this ParseResult parseResult, CliArgument argument) => parseResult.GetResult(argument).ThrowIfNull();
+
+    /// <summary>
+    /// Gets the result for the specified command.
+    /// </summary>
+    /// <param name="parseResult">The parse result.</param>
+    /// <param name="command">The command for which to find a result.</param>
+    /// <returns>A result for the specified command.</returns>
+    /// <exception cref="ArgumentNullException">Failed to find the result.</exception>
+    public static Parsing.CommandResult GetRequiredResult(this ParseResult parseResult, CliCommand command) => parseResult.GetResult(command).ThrowIfNull();
+
+    /// <summary>
+    /// Gets the result for the specified option.
+    /// </summary>
+    /// <param name="parseResult">The parse result.</param>
+    /// <param name="option">The option for which to find a result.</param>
+    /// <returns>A result for the specified option.</returns>
+    /// <exception cref="ArgumentNullException">Failed to find the result.</exception>
+    public static Parsing.OptionResult GetRequiredResult(this ParseResult parseResult, CliOption option) => parseResult.GetResult(option).ThrowIfNull();
+
+    /// <summary>
+    /// Gets the result for the specified directive.
+    /// </summary>
+    /// <param name="parseResult">The parse result.</param>
+    /// <param name="directive">The directive for which to find a result.</param>
+    /// <returns>A result for the specified directive.</returns>
+    /// <exception cref="ArgumentNullException">Failed to find the result.</exception>
+    public static Parsing.DirectiveResult GetRequiredResult(this ParseResult parseResult, CliDirective directive) => parseResult.GetResult(directive).ThrowIfNull();
+
+    /// <summary>
+    /// Gets the result for the specified symbol.
+    /// </summary>
+    /// <param name="parseResult">The parse result.</param>
+    /// <param name="symbol">The symbol for which to find a result.</param>
+    /// <returns>A result for the specified symbol.</returns>
+    /// <exception cref="ArgumentNullException">Failed to find the result.</exception>
+    public static Parsing.SymbolResult? GetRequiredResult(this ParseResult parseResult, CliSymbol symbol) => parseResult.GetResult(symbol).ThrowIfNull();
+
+    /// <summary>
+    /// Gets the command, if any, from the symbol result.
     /// </summary>
     /// <param name="symbolResult">The symbol result.</param>
-    /// <returns>The command if found.</returns>
+    /// <returns>A command for the specified symbol, or <see langword="null"/> if it was not provided and no default was configured.</returns>
     public static CliCommand? GetCommand(this Parsing.SymbolResult symbolResult)
     {
         return GetCommandCore(symbolResult);
@@ -30,4 +111,16 @@ public static class ExtensionMethods
             };
         }
     }
+
+    /// <summary>
+    /// Gets the command from the symbol result.
+    /// </summary>
+    /// <param name="symbolResult">The symbol result.</param>
+    /// <returns>A command for the specified symbol.</returns>
+    /// <exception cref="ArgumentNullException">Failed to find the result.</exception>
+    public static CliCommand GetRequiredCommand(this Parsing.SymbolResult symbolResult) => symbolResult.GetCommand().ThrowIfNull();
+
+    [Runtime.CompilerServices.MethodImpl(Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
+    private static T ThrowIfNull<T>(this T? value)
+        where T : notnull => value ?? throw new ArgumentNullException(nameof(value));
 }
---
+++ src/Tests/CommandLine.Tests/ExtensionMethodsTests.cs
@@ -0,0 +1,48 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ServiceExtensionsTests.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.CommandLine.Services;
+
+public class ExtensionMethodsTests
+{
+    [Fact]
+    public void GetRequiredValueFromOption()
+    {
+        var option = new CliOption<string>("--option");
+        var configuration = new CliConfiguration(new CliRootCommand { option });
+
+        var parseResult = configuration.Parse("--option value");
+        parseResult.GetRequiredValue(option).Should().Be("value");
+    }
+
+    [Fact]
+    public void GetRequiredValueFromOptionWhenMissing()
+    {
+        var option = new CliOption<string>("--option");
+        var configuration = new CliConfiguration(new CliRootCommand { option });
+
+        configuration.Parse(string.Empty).Invoking(parseResult => parseResult.GetRequiredValue(option)).Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void GetRequiredValueFromArgument()
+    {
+        var argument = new CliArgument<string>("ARG");
+        var configuration = new CliConfiguration(new CliRootCommand { argument });
+
+        var parseResult = configuration.Parse("value");
+        parseResult.GetRequiredValue(argument).Should().Be("value");
+    }
+
+    [Fact]
+    public void GetRequiredValueFromArgumentWhenMissing()
+    {
+        var argument = new CliArgument<string>("ARG") { DefaultValueFactory = _ => default! };
+        var configuration = new CliConfiguration(new CliRootCommand { argument });
+
+        configuration.Parse(string.Empty).Invoking(parseResult => parseResult.GetRequiredValue(argument)).Should().Throw<ArgumentNullException>();
+    }
+}
