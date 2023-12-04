dbd3fa3bae7159f965fa06b598802fcbe75c3362
Ross King <ross.king@altavec.com>
2023-12-04 11:54:05 +1100

    ✅ add `DepedencyInjection` tests

---
+++ src/Tests/CommandLine.DependencyInjection.Tests/ServicesExtensionsTests.cs
@@ -0,0 +1,52 @@
+// -----------------------------------------------------------------------
+// <copyright file="UnitTest1.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.CommandLine.DependencyInjection.Tests;
+
+using Microsoft.Extensions.DependencyInjection;
+
+public class ServicesExtensionsTests
+{
+    [Fact]
+    public void GetServices()
+    {
+        IServiceProvider? serviceProvider = default;
+        var rootCommand = new CliRootCommand();
+        rootCommand.SetAction(result => serviceProvider = result.GetServices());
+
+        var configuration = new CliConfiguration(rootCommand);
+        _ = configuration.UseServices(configure => { });
+
+        _ = configuration.Invoke(Array.Empty<string>());
+        _ = serviceProvider.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void GetService()
+    {
+        IDisposable? disposable = default;
+        var rootCommand = new CliRootCommand();
+        rootCommand.SetAction(result =>
+        {
+            if (result.GetServices() is { } serviceProvider)
+            {
+                disposable = (IDisposable?)serviceProvider.GetService(typeof(IDisposable));
+            }
+        });
+
+        var configuration = new CliConfiguration(rootCommand);
+        _ = configuration.UseServices(configure => configure.AddSingleton<IDisposable, Disposable>());
+
+        _ = configuration.Invoke(Array.Empty<string>());
+        _ = disposable.Should().NotBeNull();
+
+    }
+
+    private sealed class Disposable : IDisposable
+    {
+        void IDisposable.Dispose() => throw new NotImplementedException();
+    }
+}
