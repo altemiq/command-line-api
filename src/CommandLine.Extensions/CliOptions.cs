ff249312e81fb0a16b1eaf8dac8592cf3ea0210f
Ross King <ross.king@altavec.com>
2024-06-03 13:33:25 +1000

    ✨ add extensions for parsing, etc

---
+++ src/CommandLine.Extensions/CliOptions.cs
@@ -0,0 +1,240 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CliOptions.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.CommandLine.Extensions;
+
+/// <summary>
+/// The <see cref="CliOption"/> values.
+/// </summary>
+public static class CliOptions
+{
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+    private static readonly string[] Separator = [".."];
+
+    /// <summary>
+    /// Parses the range.
+    /// </summary>
+    /// <param name="argumentResult">The argument result.</param>
+    /// <returns>The parsed value.</returns>
+    public static Range ParseRange(Parsing.ArgumentResult argumentResult) => ParseRange(argumentResult.Tokens[^1]);
+
+    /// <summary>
+    /// Parses the range.
+    /// </summary>
+    /// <param name="token">The token.</param>
+    /// <returns>The parsed value.</returns>
+    public static Range ParseRange(Parsing.CliToken token) => ParseRange(token.Value);
+
+    /// <summary>
+    /// Parses the range.
+    /// </summary>
+    /// <param name="value">The string to parse.</param>
+    /// <returns>The parsed value.</returns>
+    public static Range ParseRange(string value)
+    {
+        // split on the '..'
+        var split = value.Split(Separator, StringSplitOptions.None);
+
+        // do the start and end
+        var startString = split[0];
+        var endString = split[1];
+
+        var startIndex = string.IsNullOrEmpty(startString)
+            ? Index.Start
+            : ParseIndex(startString);
+
+        var endIndex = string.IsNullOrEmpty(endString)
+            ? Index.End
+            : ParseIndex(endString);
+
+        return new Range(startIndex, endIndex);
+    }
+
+    /// <summary>
+    /// Parses the ranges.
+    /// </summary>
+    /// <param name="argumentResult">The argument result.</param>
+    /// <returns>The parsed values.</returns>
+    public static Range[] ParseRanges(Parsing.ArgumentResult argumentResult) => ParseRanges(argumentResult.Tokens);
+
+    /// <summary>
+    /// Parses the ranges.
+    /// </summary>
+    /// <param name="tokens">The tokens.</param>
+    /// <returns>The parsed values.</returns>
+    public static Range[] ParseRanges(IEnumerable<Parsing.CliToken> tokens) => ParseRanges(tokens.Select(token => token.Value));
+
+    /// <summary>
+    /// Parses the ranges.
+    /// </summary>
+    /// <param name="values">The strings to parse.</param>
+    /// <returns>The parsed values.</returns>
+    public static Range[] ParseRanges(IEnumerable<string> values) => values.Select(ParseRange).ToArray();
+
+    /// <summary>
+    /// Parses the index.
+    /// </summary>
+    /// <param name="argumentResult">The argument result.</param>
+    /// <returns>The parsed value.</returns>
+    public static Index ParseIndex(Parsing.ArgumentResult argumentResult) => ParseIndex(argumentResult.Tokens[^1]);
+
+    /// <summary>
+    /// Parses the index.
+    /// </summary>
+    /// <param name="token">The token.</param>
+    /// <returns>The parsed value.</returns>
+    public static Index ParseIndex(Parsing.CliToken token) => ParseIndex(token.Value);
+
+    /// <summary>
+    /// Parses the index.
+    /// </summary>
+    /// <param name="value">The string to parse.</param>
+    /// <returns>The parsed value.</returns>
+    public static Index ParseIndex(string value)
+    {
+        var fromEnd = value.StartsWith('^');
+        return new Index(int.Parse(fromEnd ? value[1..] : value, System.Globalization.CultureInfo.InvariantCulture), fromEnd);
+    }
+
+    /// <summary>
+    /// Parses the indexes.
+    /// </summary>
+    /// <param name="argumentResult">The argument result.</param>
+    /// <returns>The parsed value.</returns>
+    public static Index[] ParseIndexes(Parsing.ArgumentResult argumentResult) => ParseIndexes(argumentResult.Tokens);
+
+    /// <summary>
+    /// Parses the indexes.
+    /// </summary>
+    /// <param name="tokens">The tokens to parse.</param>
+    /// <returns>The parsed value.</returns>
+    public static Index[] ParseIndexes(IEnumerable<Parsing.CliToken> tokens) => ParseIndexes(tokens.Select(token => token.Value));
+
+    /// <summary>
+    /// Parses the indexes.
+    /// </summary>
+    /// <param name="values">The strings to parse.</param>
+    /// <returns>The parsed value.</returns>
+    public static Index[] ParseIndexes(IEnumerable<string> values) => values.Select(ParseIndex).ToArray();
+#endif
+
+    /// <summary>
+    /// Parses the file system globbing.
+    /// </summary>
+    /// <param name="argumentResult">The argument result.</param>
+    /// <param name="directoryInfo">The directory information.</param>
+    /// <returns>The files matched by the globbing.</returns>
+    public static FileInfo[] ParseGlobbing(Parsing.ArgumentResult argumentResult, Microsoft.Extensions.FileSystemGlobbing.Abstractions.DirectoryInfoBase? directoryInfo = default) => ParseGlobbing(argumentResult.Tokens, directoryInfo);
+
+    /// <summary>
+    /// Parses the file system globbing.
+    /// </summary>
+    /// <param name="tokens">The tokens.</param>
+    /// <param name="directoryInfo">The directory information.</param>
+    /// <returns>The files matched by the globbing.</returns>
+    public static FileInfo[] ParseGlobbing(IEnumerable<Parsing.CliToken> tokens, Microsoft.Extensions.FileSystemGlobbing.Abstractions.DirectoryInfoBase? directoryInfo = default) => ParseGlobbing(tokens.Select(token => token.Value), directoryInfo);
+
+    /// <summary>
+    /// Parses the file system globbing.
+    /// </summary>
+    /// <param name="globs">The globs.</param>
+    /// <param name="directoryInfo">The directory information.</param>
+    /// <returns>The files matched by the globbing.</returns>
+    public static FileInfo[] ParseGlobbing(IEnumerable<string> globs, Microsoft.Extensions.FileSystemGlobbing.Abstractions.DirectoryInfoBase? directoryInfo = default)
+    {
+        return Process(globs, directoryInfo).SelectMany(results => results.Select(file => new FileInfo(file))).ToArray();
+
+        static IEnumerable<IEnumerable<string>> Process(IEnumerable<string> tokens, Microsoft.Extensions.FileSystemGlobbing.Abstractions.DirectoryInfoBase? directoryInfo)
+        {
+            var patternBuilder = new Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns.PatternBuilder(StringComparison.OrdinalIgnoreCase);
+            var normalisedTokens = tokens.Select(NormalizedName).ToArray();
+            var rooted = normalisedTokens.Where(Path.IsPathRooted).ToArray();
+            foreach (var root in rooted)
+            {
+                yield return GetRooted(root, directoryInfo, patternBuilder);
+            }
+
+            yield return GetFiles(directoryInfo ?? GetDirectoryInfo(Environment.CurrentDirectory), patternBuilder, normalisedTokens.Except(rooted, StringComparer.Ordinal).ToArray());
+
+            static IEnumerable<string> GetRooted(string root, Microsoft.Extensions.FileSystemGlobbing.Abstractions.DirectoryInfoBase? directoryInfo, Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns.PatternBuilder patternBuilder)
+            {
+                // find the first '*' element
+                var starIndex =
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+                    root.IndexOf('*', StringComparison.Ordinal);
+#else
+                    root.IndexOf('*');
+#endif
+                if (starIndex < 0)
+                {
+                    yield return root;
+                    yield break;
+                }
+
+                // trace back to the directory
+                string pattern;
+                string directoryPath;
+                var directorySeparatorIndex = root.LastIndexOf(Path.AltDirectorySeparatorChar, starIndex);
+                if (directorySeparatorIndex < 0)
+                {
+                    // there is no root below this
+                    pattern = root;
+                    directoryPath = string.Empty;
+                }
+                else
+                {
+#if NETSTANDARD2_0
+                    directoryPath = root.Substring(0, directorySeparatorIndex);
+                    pattern = root.Substring(directorySeparatorIndex + 1);
+#else
+                    directoryPath = root[..directorySeparatorIndex];
+                    pattern = root[(directorySeparatorIndex + 1)..];
+#endif
+                }
+
+                if (directoryInfo is null || !string.Equals(NormalizedName(directoryInfo.FullName), directoryPath, StringComparison.Ordinal))
+                {
+                    directoryInfo = GetDirectoryInfo(directoryPath);
+                }
+
+                foreach (var match in GetFiles(directoryInfo!, patternBuilder, pattern))
+                {
+                    yield return match;
+                }
+            }
+
+            static IEnumerable<string> GetFiles(Microsoft.Extensions.FileSystemGlobbing.Abstractions.DirectoryInfoBase directoryInfo, Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns.PatternBuilder patternBuilder, params string[] includePatterns)
+            {
+                if (includePatterns.Length == 0)
+                {
+                    yield break;
+                }
+
+                var context = new Microsoft.Extensions.FileSystemGlobbing.Internal.MatcherContext(
+                    includePatterns.Select(includePattern => patternBuilder.Build(includePattern)),
+                    [],
+                    directoryInfo,
+                    patternBuilder.ComparisonType);
+                var result = context.Execute();
+                var directoryName = directoryInfo.FullName;
+                foreach (var match in result.Files)
+                {
+                    yield return Path.GetFullPath(Path.Combine(directoryName, match.Path));
+                }
+            }
+
+            static Microsoft.Extensions.FileSystemGlobbing.Abstractions.DirectoryInfoBase GetDirectoryInfo(string directoryPath)
+            {
+                return new Microsoft.Extensions.FileSystemGlobbing.Abstractions.DirectoryInfoWrapper(new DirectoryInfo(directoryPath));
+            }
+        }
+
+        static string NormalizedName(string name)
+        {
+            return name.Replace(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+        }
+    }
+}
---
+++ src/Tests/CommandLine.Extensions.Tests/CliOptionsTests.cs
@@ -0,0 +1,176 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CliOptionsTests.cs" company="Altavec">
+// Copyright (c) Altavec. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.CommandLine.Extensions;
+
+public class CliOptionsTests
+{
+    private static readonly CliCommand command = new("base") { new CliArgument<string>("RANGE") };
+
+    public static TheoryData<string, SerializableRange> Ranges() => new()
+    {
+        { "..", new(new Range(start: Index.Start, end: Index.End)) },
+        { "0..", new(new Range(start: Index.Start, end: Index.End)) },
+        { "..^0", new(new Range(start: Index.Start, end: Index.End)) },
+        { "0..^0", new(new Range(start: Index.Start, end: Index.End)) },
+        { "1..", new(new Range(start: new Index(value: 1), end: Index.End)) },
+        { "3..^5", new(new Range(start: new Index(value: 3), end: new Index(value: 5, fromEnd: true))) },
+        { "^5..^2", new(new Range(start: new Index(value: 5, fromEnd: true), end: new Index(value: 2, fromEnd: true))) },
+    };
+
+    [Fact]
+    public void FileSystemGlobbing()
+    {
+        const string RootDir = "C:\\Files to Search";
+        var first = Path.Join(RootDir, "first", "first.las");
+        var second = Path.Join(RootDir, "second.las");
+        var third = Path.Join(RootDir, "deep", "deep", "path", "third.las");
+        var forth = Path.Join(RootDir, "forth.laz");
+
+        var directoryInfo = new Microsoft.Extensions.FileSystemGlobbing.InMemoryDirectoryInfo("C:\\Files to Search", [first, second, third, forth]);
+
+        var argument = new CliArgument<FileInfo[]>("FILES") { CustomParser = argumentResult => CliOptions.ParseGlobbing(argumentResult, directoryInfo) };
+        var root = new CliRootCommand { argument };
+        var configuration = new CliConfiguration(root);
+        var parseResult = configuration.Parse("\"" + Path.Combine(RootDir, "**", "*.las") + "\"");
+
+        parseResult.GetValue(argument).Should().NotBeNull()
+            .And.Subject.Select(x => x.FullName).Should()
+                .Contain(first).And
+                .Contain(second).And
+                .Contain(third).And
+                .NotContain(forth);
+
+    }
+
+    [Fact]
+    public void TestSerializableRange()
+    {
+        var serializable = new SerializableRange(new Range(start: Index.Start, end: Index.End));
+        var serialized = Xunit.Sdk.SerializationHelper.Serialize(serializable);
+        var deserialized = Xunit.Sdk.SerializationHelper.Deserialize<SerializableRange>(serialized);
+        _ = deserialized.Should().Be(serializable);
+    }
+
+
+    [Fact]
+    public void TestSerializableIndex()
+    {
+        var serializable = new SerializableIndex(new Index(1, true));
+        var serialized = Xunit.Sdk.SerializationHelper.Serialize(serializable);
+        var deserialized = Xunit.Sdk.SerializationHelper.Deserialize<SerializableIndex>(serialized);
+        _ = deserialized.Should().Be(serializable);
+    }
+
+    [Theory]
+    [MemberData(nameof(Ranges))]
+    public void ParseRange(string input, SerializableRange expected) => CliOptions.ParseRange(input).Should().Be(expected, RangeEqualityComparer.Instance);
+
+    [Theory]
+    [MemberData(nameof(Ranges))]
+    public void ParseOption(string input, SerializableRange expected) => CliOptions.ParseRange(command.Parse(input).CommandResult.Children.FirstOrDefault().As<Parsing.ArgumentResult>()).Should().Be(expected, RangeEqualityComparer.Instance);
+
+    private class RangeEqualityComparer : IEqualityComparer<Range>
+    {
+        public static readonly IEqualityComparer<Range> Instance = new RangeEqualityComparer();
+
+        public bool Equals(Range x, Range y) => x.Equals(y);
+
+        public int GetHashCode([Diagnostics.CodeAnalysis.DisallowNull] Range obj) => obj.GetHashCode();
+    }
+
+    private class IndexEqualityComparer : IEqualityComparer<Index>
+    {
+        public static readonly IEqualityComparer<Index> Instance = new IndexEqualityComparer();
+
+        public bool Equals(Index x, Index y) => x.Equals(y);
+
+        public int GetHashCode([Diagnostics.CodeAnalysis.DisallowNull] Index obj) => obj.GetHashCode();
+    }
+
+    public class SerializableRange : Xunit.Abstractions.IXunitSerializable
+    {
+        private Range range;
+
+        public SerializableRange()
+        {
+        }
+
+        public SerializableRange(Range range) => this.range = range;
+
+        public void Deserialize(Xunit.Abstractions.IXunitSerializationInfo info)
+        {
+            var startValue = info.GetValue<int>($"{nameof(this.range)}.{nameof(this.range.Start)}.{nameof(this.range.Start.Value)}");
+            var startIsFromEnd = info.GetValue<bool>($"{nameof(this.range)}.{nameof(this.range.Start)}.{nameof(this.range.Start.IsFromEnd)}");
+            var endValue = info.GetValue<int>($"{nameof(this.range)}.{nameof(this.range.End)}.{nameof(this.range.End.Value)}");
+            var endIsFromEnd = info.GetValue<bool>($"{nameof(this.range)}.{nameof(this.range.End)}.{nameof(this.range.End.IsFromEnd)}");
+
+            this.range = new Range(new Index(startValue, startIsFromEnd), new Index(endValue, endIsFromEnd));
+        }
+
+        public void Serialize(Xunit.Abstractions.IXunitSerializationInfo info)
+        {
+            info.AddValue($"{nameof(this.range)}.{nameof(this.range.Start)}.{nameof(this.range.Start.Value)}", this.range.Start.Value, typeof(int));
+            info.AddValue($"{nameof(this.range)}.{nameof(this.range.Start)}.{nameof(this.range.Start.IsFromEnd)}", this.range.Start.IsFromEnd, typeof(bool));
+            info.AddValue($"{nameof(this.range)}.{nameof(this.range.End)}.{nameof(this.range.End.Value)}", this.range.End.Value, typeof(int));
+            info.AddValue($"{nameof(this.range)}.{nameof(this.range.End)}.{nameof(this.range.End.IsFromEnd)}", this.range.End.IsFromEnd, typeof(bool));
+        }
+
+        public static implicit operator Range(SerializableRange range) => range.range;
+
+        public static implicit operator SerializableRange(Range range) => new(range);
+
+        public override string ToString() => this.range.ToString();
+
+        public override bool Equals(object? obj) => obj switch
+        {
+            SerializableRange range => RangeEqualityComparer.Instance.Equals(this.range, range.range),
+            Range range => RangeEqualityComparer.Instance.Equals(this.range, range),
+            _ => base.Equals(obj),
+        };
+
+        public override int GetHashCode() => RangeEqualityComparer.Instance.GetHashCode(this);
+    }
+
+    public class SerializableIndex : Xunit.Abstractions.IXunitSerializable
+    {
+        private Index index;
+
+        public SerializableIndex()
+        {
+        }
+
+        public SerializableIndex(Index index) => this.index = index;
+
+        public void Deserialize(Xunit.Abstractions.IXunitSerializationInfo info)
+        {
+            var value = info.GetValue<int>($"{nameof(this.index)}.{nameof(this.index.Value)}");
+            var isFromEnd = info.GetValue<bool>($"{nameof(this.index)}.{nameof(this.index.IsFromEnd)}");
+            this.index = new Index(value, isFromEnd);
+        }
+
+        public void Serialize(Xunit.Abstractions.IXunitSerializationInfo info)
+        {
+            info.AddValue($"{nameof(this.index)}.{nameof(this.index.Value)}", this.index.Value, typeof(int));
+            info.AddValue($"{nameof(this.index)}.{nameof(this.index.IsFromEnd)}", this.index.IsFromEnd, typeof(bool));
+        }
+
+        public static implicit operator Index(SerializableIndex index) => index.index;
+
+        public static implicit operator SerializableIndex(Index index) => new(index);
+
+        public override string ToString() => this.index.ToString();
+
+        public override bool Equals(object? obj) => obj switch
+        {
+            SerializableIndex index => IndexEqualityComparer.Instance.Equals(this.index, index.index),
+            Index index => IndexEqualityComparer.Instance.Equals(this.index, index),
+            _ => base.Equals(obj),
+        };
+
+        public override int GetHashCode() => IndexEqualityComparer.Instance.GetHashCode(this);
+    }
+}
