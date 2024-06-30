60df8f30b2cbe671a899aba1ff92eb93ea4069c3
Ross King <ross.king@altavec.com>
2024-07-01 09:49:45 +1000

    🧑‍💻 remove attributes from code coverage

--- src/CommandLine/System.Diagnostics.CodeAnalysis.cs
+++ src/CommandLine/System.Diagnostics.CodeAnalysis.cs
@@ -10,42 +10,54 @@
 
 namespace System.Diagnostics.CodeAnalysis;
 
+[ExcludeFromCodeCoverage]
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
 internal sealed class AllowNullAttribute : Attribute;
 
+[ExcludeFromCodeCoverage]
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
 internal sealed class DisallowNullAttribute : Attribute;
 
+[ExcludeFromCodeCoverage]
 [AttributeUsage(AttributeTargets.Method, Inherited = false)]
 internal sealed class DoesNotReturnAttribute : Attribute;
 
+[ExcludeFromCodeCoverage]
 [AttributeUsage(AttributeTargets.Parameter)]
 internal sealed class DoesNotReturnIfAttribute(bool parameterValue) : Attribute
 {
     public bool ParameterValue => parameterValue;
 }
 
+#if !NETSTANDARD2_0_OR_GREATER && !NET40_OR_GREATER && !NETCOREAPP2_0_OR_GREATER
+[ExcludeFromCodeCoverage]
 [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Event | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
 internal sealed class ExcludeFromCodeCoverageAttribute : Attribute;
+#endif
 
+[ExcludeFromCodeCoverage]
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
 internal sealed class MaybeNullAttribute : Attribute;
 
+[ExcludeFromCodeCoverage]
 [AttributeUsage(AttributeTargets.Parameter)]
 internal sealed class MaybeNullWhenAttribute(bool returnValue) : Attribute
 {
     public bool ReturnValue => returnValue;
 }
 
+[ExcludeFromCodeCoverage]
 [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
 internal sealed class NotNullAttribute : Attribute;
 
+[ExcludeFromCodeCoverage]
 [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, AllowMultiple = true, Inherited = false)]
 internal sealed class NotNullIfNotNullAttribute(string parameterName) : Attribute
 {
     public string ParameterName => parameterName;
 }
 
+[ExcludeFromCodeCoverage]
 [AttributeUsage(AttributeTargets.Parameter)]
 internal sealed class NotNullWhenAttribute(bool returnValue) : Attribute
 {
