dd85ca7c5ed0840096ce5c25465796c5f7f6245d
Ross King <ross.king@altavec.com>
2024-08-23 10:45:14 +1000

    💚 fix spectre formatting errors

---
+++ src/CommandLine.Spectre/AnsiConsoleProgressItem.Record.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AnsiConsoleProgressItem.Record.cs" company="Altemiq">
+// Copyright (c) Altemiq. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace System.CommandLine;
+
+/// <summary>
+/// The <see cref="IAnsiConsole"/> percentage item.
+/// </summary>
+/// <param name="Name">The name.</param>
+/// <param name="Percentage">
+///   <para>The percentage.</para>
+///   <list type="table">
+///     <listheader>
+///       <term>Value</term>
+///       <description>Meaning</description>
+///     </listheader>
+///     <item>
+///       <term><see cref="double.PositiveInfinity"/></term>
+///       <description>Sets <see cref="ProgressTask.IsIndeterminate"/> to <see langword="true"/></description>
+///     </item>
+///     <item>
+///       <term><see cref="double.NaN"/></term>
+///       <description>Comletes the <see cref="ProgressTask"/> without setting <see cref="ProgressTask.Value"/>.</description>
+///     </item>
+///     <item>
+///       <term>greater than or equal to 100</term>
+///       <description>Completes the <see cref="ProgressTask"/> and sets <see cref="ProgressTask.Value"/> to 100%.</description>
+///     </item>
+///   </list>
+/// </param>
+public readonly record struct AnsiConsoleProgressItem(string Name, double Percentage);
--- src/CommandLine.Spectre/AnsiConsoleProgressItem.Struct.cs
+++ src/CommandLine.Spectre/AnsiConsoleProgressItem.Struct.cs
@@ -1,39 +1,11 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="AnsiConsoleProgressItem.cs" company="Altemiq">
+// <copyright file="AnsiConsoleProgressItem.Struct.cs" company="Altemiq">
 // Copyright (c) Altemiq. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
 
 namespace System.CommandLine;
 
-#if NET5_0_OR_GREATER
-/// <summary>
-/// The <see cref="IAnsiConsole"/> percentage item.
-/// </summary>
-/// <param name="Name">The name.</param>
-/// <param name="Percentage">
-///   <para>The percentage.</para>
-///   <list type="table">
-///     <listheader>
-///       <term>Value</term>
-///       <description>Meaning</description>
-///     </listheader>
-///     <item>
-///       <term><see cref="double.PositiveInfinity"/></term>
-///       <description>Sets <see cref="ProgressTask.IsIndeterminate"/> to <see langword="true"/></description>
-///     </item>
-///     <item>
-///       <term><see cref="double.NaN"/></term>
-///       <description>Comletes the <see cref="ProgressTask"/> without setting <see cref="ProgressTask.Value"/>.</description>
-///     </item>
-///     <item>
-///       <term>greater than or equal to 100</term>
-///       <description>Completes the <see cref="ProgressTask"/> and sets <see cref="ProgressTask.Value"/> to 100%.</description>
-///     </item>
-///   </list>
-/// </param>
-public readonly record struct AnsiConsoleProgressItem(string Name, double Percentage);
-#else
 /// <summary>
 /// The <see cref="IAnsiConsole"/> percentage item.
 /// </summary>
@@ -89,4 +61,3 @@ public readonly struct AnsiConsoleProgressItem(string name, double percentage)
     /// </summary>
     public double Percentage { get; } = percentage;
 }
-#endif
