1f65e53ca3498fdf44cb952e698e948288cbbe96
Ross King <rsking@gmail.com>
2025-11-10 11:17:10 +1100

    👽️ update to .NET 10.0

--- tests/CommandLine.Tests/OptionsTests.cs
+++ tests/CommandLine.Tests/OptionsTests.cs
@@ -11,9 +11,11 @@ public class OptionsTests
     [Test]
     public async Task Verbosity()
     {
+#pragma warning disable CS8634 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'class' constraint.
         _ = await Assert.That(new VerbosityOption())
             .Member(verbosity => verbosity.Recursive, recursive => recursive.IsTrue()).And
             .Member(verbosity => verbosity.HasDefaultValue, hasDefaultValue => hasDefaultValue.IsTrue()).And
             .Member(verbosity => verbosity.DefaultValueFactory, defaultValueFactory => defaultValueFactory.IsNotNull());
+#pragma warning restore CS8634 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'class' constraint.
     }
 }
